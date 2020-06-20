a9d72166e2aaf46d0ea261cf29f9798004c05d21
Sandmann79 <sandmann79@gmx.net>
2020-06-20 16:12:30 +0200

    added jni libs

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI:=armeabi-v7a 
 APP_OPTIM := debug
-APP_PLATFORM := android-18
+APP_PLATFORM := android-25
 APP_ALLOW_MISSING_DEPS:=true
 #APP_MODULES:= jpeg libpng libcrypto_static libssl_static androidvncserver
